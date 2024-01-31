5dbb8e6259a473afc71c51fda8f388b6c20485be
Neel Gajjar <nilbgajjar@gmail.com>
2024-01-31 15:24:17 +0530

    First Commit

---
+++ src/ArrayList/Q1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ArrayList;
+import java.util.*;
+/**
+ *
+ * @author Admin
+ */
+public class Q1 {
+    
+    public static void main(String []args){
+        List<String> MovieList = new ArrayList<>();
+        MovieList.add("Spider Man");
+        MovieList.add("Doctor Strange");
+        MovieList.add("John Wick");
+        MovieList.add("Wrong Turn");
+        MovieList.add("Final Destination");
+        System.out.println("MovieList : ");
+        System.out.println("            "+MovieList);
+        
+    }
+    
+}
---
+++ src/ArrayList/Q3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class Q3 {
+    
+    public static void main(String []args){
+        
+    }
+    
+}
